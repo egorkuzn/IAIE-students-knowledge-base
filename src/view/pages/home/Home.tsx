1ad466a31a23bf5d47295b9dc7f56fb5ea97f442
egorkuzn <georkuzn2002@mail.ru>
2025-02-05 11:43:56 +0700

    home: завершение разработки домашней страницы

--- src/view/pages/home/Home.tsx
+++ src/view/pages/home/Home.tsx
@@ -1,4 +1,6 @@
 import App from "../../../App";
+import '../../../styles/pages/Home.scss';
+import video from '../../../styles/pages/background-video.mp4'
 
 export function Home(props: any) {
     return (
@@ -9,8 +11,16 @@ export function Home(props: any) {
 function page() {
     return (
         <main>
-            <div className="home">
-            <h1>Процесс-ориентированное программирование – эффективная технология разработки управляющих программ в виде набора взаимодействующих процессов</h1>
+            <div className="content">
+                <video autoPlay muted loop id="backgroundVideo">
+                    <source src={video} type={"video/mp4"}/>
+                </video>
+                <div className="home">
+                    <div className="text-block">
+                        <h1>Процесс-ориентированное программирование – </h1>
+                        <h2>эффективная технология разработки управляющих программ в виде набора взаимодействующих процессов</h2>
+                    </div>
+                </div>
             </div>
         </main>
     )
