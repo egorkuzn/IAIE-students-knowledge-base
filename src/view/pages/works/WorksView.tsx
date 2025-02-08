b136d11aaeffa6db5cfcbbc87159556688325ca2
egorkuzn <georkuzn2002@mail.ru>
2025-02-08 20:02:07 +0700

    works: Создание стиля и завершение страницы

--- src/view/pages/works/WorksView.tsx
+++ src/view/pages/works/WorksView.tsx
@@ -2,6 +2,7 @@ import App from "../../../App";
 import {WorksClassModel} from "../../../model/pages/works/WorksClassModel";
 import {ReflexWorksClass, VerificationAndDebugPOPWorksClass} from "../../../model/pages/works/WorksClassesProperties";
 import {WorkModel} from "../../../model/pages/works/WorkModel";
+import '../../../styles/pages/Works.scss';
 
 export function WorksView(props: any) {
     return (App(page()))
@@ -15,23 +16,27 @@ function page() {
 
     return (
         <main>
-            <h1>Дипломные работы и диссертации</h1>
-            { worksClasses.map((worksClassModel: WorksClassModel) => {
-                return (
-                    <div>
-                        <h2>{worksClassModel.title}</h2>
-                        {worksClassModel.works.map((workModel: WorkModel) => {
-                            return (
-                                <div>
-                                    <p>{workModel.authors}</p>
-                                    <p>{workModel.theme}</p>
-                                    <p>{workModel.published}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
-            }) }
+            <div className="works-page">
+                <h1 className="works-style">Дипломные работы и диссертации</h1>
+                { worksClasses.map((worksClassModel: WorksClassModel) => {
+                    return (
+                        <div className="works-class">
+                            <h2 className="works-style">{worksClassModel.title}</h2>
+                            {worksClassModel.works.map((workModel: WorkModel) => {
+                                return (
+                                    <div className="work">
+                                        <div className="work-authors-titles">
+                                            <p>{workModel.authors}</p>
+                                            <p>{workModel.theme}</p>
+                                        </div>
+                                        <p className="work-published">{workModel.published}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                }) }
+            </div>
         </main>
     )
 }
