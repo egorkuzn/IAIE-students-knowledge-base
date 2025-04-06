9ab3346ddba1552b60a00a8bf96d0e866392b9db
egorkuzn <georkuzn2002@mail.ru>
2025-04-06 19:37:31 +0700

    carousel Добавлен редирект на страницу лаборатории

--- src/config/Router.tsx
+++ src/config/Router.tsx
@@ -4,7 +4,7 @@ import {VideoView} from "../view/pages/video/VideoView";
 import {PublicationsView} from "../view/pages/publications/PublicationsView";
 import {DocsView} from "../view/pages/docs/DocsView";
 import {Home} from "../view/pages/home/Home";
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, Navigate, replace} from "react-router";
 
 export const router = createBrowserRouter([
     {
@@ -34,5 +34,12 @@ export const router = createBrowserRouter([
     {
         path: "/video",
         Component: VideoView
+    },
+    {
+        path: "*",
+        Component: () => {
+            window.location.href = "https://www.iae.nsk.su/ru/laboratory-sites/lab-19"
+            return null
+        }
     }
 ]);
